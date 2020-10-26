5ac4efdbc0029323e2910dd2165f0535f82833a7
EC2 Default User <ec2-user@ip-172-31-29-195.ec2.internal>
2020-10-26 20:01:58 +0000

    Create new google user test

--- tests/mocked_unit_tests.py
+++ tests/mocked_unit_tests.py
@@ -13,8 +13,10 @@ EXPECTED_PICTURE = 'pic'
 EXPECTED_EMAIL = 'email'
 EXPECTED_ONLINE = 'online'
 EXPECTED_NAME = 'name'
+EXPECTED_USER = 'user'
 EXPECTED_COMMAND = 'name'
 EXPECTED_NAMESPACE = 'namespace'
+EXPECTED_ID = 'id'
 
 DEFAULT_NAME = 'Guest'
 DEFAULT_EMAIL = 'unknown'
@@ -37,24 +39,22 @@ class Test_Client(unittest.TestCase):
         self.assertEqual(len(response), 1)
         self.assertEqual(response[0]['args'][0]['test'], 'Connected')
         
-        EXPECTED = [{EXPECTED_COMMAND: 'current userlist', 
-        'args': [{client.sid: 
-            {EXPECTED_NAME: DEFAULT_NAME, 
+        EXPECTED = {
+            EXPECTED_NAME: DEFAULT_NAME, 
             EXPECTED_ONLINE: False, 
             EXPECTED_EMAIL: DEFAULT_EMAIL, 
-            EXPECTED_PICTURE: DEFAULT_PICTURE}}], 
-        EXPECTED_NAMESPACE:DEFAULT_NAMESPACE}]
-    
+            EXPECTED_PICTURE: DEFAULT_PICTURE}
+            
         client.emit('i am here', client.sid)
         response = client.get_received()
-        self.assertEqual(response, EXPECTED)
+        self.assertEqual(response[0]['args'][0][client.sid], EXPECTED)
         
         
         client2 = socketio.test_client(app)
         self.assertTrue(client2.is_connected())
         client2.get_received()
         
-        EXPECTED[0]['args'][0][client2.sid] = {
+        EXPECTED = {
             EXPECTED_NAME: DEFAULT_NAME, 
             EXPECTED_ONLINE: False, 
             EXPECTED_EMAIL: DEFAULT_EMAIL, 
@@ -63,7 +63,7 @@ class Test_Client(unittest.TestCase):
         
         client2.emit('i am here', client2.sid)
         response = client2.get_received()
-        self.assertEqual(response, EXPECTED)
+        self.assertEqual(response[0]['args'][0][client.sid], EXPECTED)
         
         client.disconnect()
         self.assertFalse(client.is_connected())
@@ -71,9 +71,85 @@ class Test_Client(unittest.TestCase):
         client2.disconnect()
         self.assertFalse(client2.is_connected())
   
+    def test_add_google_user(self):
+        client = socketio.test_client(app)
+        client.get_received()
+        client.emit('i am here', client.sid)
+        client.get_received()
         
+        NEW_USER = {
+            EXPECTED_ID: client.sid,
+            EXPECTED_USER: 'google_user', 
+            EXPECTED_EMAIL: 'gu@gmail.com', 
+            EXPECTED_PICTURE: 'testpic.png',
+        }
+        
+        EXPECTED = [{EXPECTED_COMMAND: 'current userlist', 
+        'args': [{client.sid: 
+            {EXPECTED_NAME: NEW_USER[EXPECTED_USER], 
+            EXPECTED_ONLINE: True, 
+            EXPECTED_EMAIL: NEW_USER[EXPECTED_EMAIL], 
+            EXPECTED_PICTURE: NEW_USER[EXPECTED_PICTURE]}}], 
+        EXPECTED_NAMESPACE:DEFAULT_NAMESPACE},
+        {EXPECTED_COMMAND: 'current user', 
+        'args': [{client.sid: 
+            {EXPECTED_NAME: NEW_USER[EXPECTED_USER], 
+            EXPECTED_ONLINE: True, 
+            EXPECTED_EMAIL: NEW_USER[EXPECTED_EMAIL], 
+            EXPECTED_PICTURE: NEW_USER[EXPECTED_PICTURE]}}], 
+        EXPECTED_NAMESPACE:DEFAULT_NAMESPACE}]
+        
+        client.emit('new google user', NEW_USER)
+        
+        response = client.get_received()
+        
+        self.assertEqual(response[0], EXPECTED[0])
+        self.assertEqual(response[1], EXPECTED[1])
+        
+        client.emit('get userlist')
+        response = client.get_received()
+        self.assertEqual(len(response),1)
+        print(client.sid)
+        client.disconnect()
+        self.assertFalse(client.is_connected())
+        
+        
+        
+        
+    
+    """
+    def test_send_message(self):
+        client = socketio.test_client(app)
+        response = client.get_received()
+        
+        client.emit('i am here', client.sid)
+        response = client.get_received()
+        
+        
+        MESSAGE = {'message': 'This is a simple test message',
+            'id' : client.sid,
+        }
+        
+        MOCKED_MESSAGES = {
+            
+        }
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
         
 
+        db.create_all() 
+        db.session.commit()
+        db.Messages.query.all()
+           
+        client.emit('send message', MESSAGE)
+        response = client.get_received()
+        
+        print(response)
+        print(client.sid)
+        
+        
+        client.emit('send message', MESSAGE)
+        response = client.get_received()
+        print(response)
 
 
    # def test_socketio(self):
@@ -85,6 +161,6 @@ class Test_Client(unittest.TestCase):
       #  r = flask_test_client.on('connect')
       #  print(r.status_code)
       #  assert r.status_code == 200
-        
+    """
 if __name__ == '__main__':
     unittest.main()
