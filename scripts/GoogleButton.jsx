d80aa1841d1f6ccdc0b1aab89fea602c7100496e
EC2 Default User <ec2-user@ip-172-31-29-195.ec2.internal>
2020-10-27 23:44:03 +0000

    Eslint fix jsx files

--- scripts/GoogleButton.jsx
+++ scripts/GoogleButton.jsx
@@ -1,39 +1,37 @@
 import * as React from 'react';
-import { Socket } from './Socket';
-import ReactDOM from 'react-dom';
 import GoogleLogin from 'react-google-login';
+import { Socket } from './Socket';
 
 function handleSubmit(response) {
-    // TODO replace with name from oauth
-    let name = response.profileObj.givenName;
-    let email = response.profileObj.email;
-    let profilePicture = response.profileObj.imageUrl;
-    
-    console.log(name);
-    Socket.emit('new google user', {
-        'id': Socket.id,
-        'user': name,
-        'email':email,
-        'pic': profilePicture,
-    });
+  // TODO replace with name from oauth
+  const name = response.profileObj.givenName;
+  const { email } = response.profileObj;
+  const profilePicture = response.profileObj.imageUrl;
+
+  Socket.emit('new google user', {
+    id: Socket.id,
+    user: name,
+    email,
+    pic: profilePicture,
+  });
 }
 
-export function GoogleButton() {
-    return <GoogleLogin
-        clientId="829909914851-6dnd3po5vfkgg3nbvm2jc75n31g9vvc0.apps.googleusercontent.com"
-        render={renderProps => (
-          <button onClick={renderProps.onClick} disabled={renderProps.disabled}>Login!</button>
-        )}
-        buttonText="Login"
-        onSuccess={handleSubmit}
-        onFailure={handleSubmit}
-        cookiePolicy={'single_host_origin'} />;
+export default function GoogleButton() {
+  return (
+    <GoogleLogin
+      clientId="829909914851-6dnd3po5vfkgg3nbvm2jc75n31g9vvc0.apps.googleusercontent.com"
+      render={(renderProps) => (
+        <button type="submit" onClick={renderProps.onClick} disabled={renderProps.disabled}>Login!</button>
+      )}
+      buttonText="Login"
+      onSuccess={handleSubmit}
+      onFailure={handleSubmit}
+      cookiePolicy="single_host_origin"
+    />
+  );
 }
 
-
-
-//client ID
-//829909914851-6dnd3po5vfkgg3nbvm2jc75n31g9vvc0.apps.googleusercontent.com
-//client secret
-//N9TyWZBmrxyBh4YR6TQcZT38
-
+// client ID
+// 829909914851-6dnd3po5vfkgg3nbvm2jc75n31g9vvc0.apps.googleusercontent.com
+// client secret
+// N9TyWZBmrxyBh4YR6TQcZT38
--- scripts/MessageList.jsx
+++ scripts/MessageList.jsx
@@ -2,47 +2,47 @@ import * as React from 'react';
 import Parser from 'html-react-parser';
 import { Socket } from './Socket';
 
-export function MessageList() {
-    const [messages, setMessages] = React.useState(() => {
-        Socket.emit('get messages');
-        return [];
-    });
-    
-    React.useEffect(() => {
-        Socket.on('messages updated', updateMessages);
-        return () => {
-            Socket.off('messages updated', updateMessages);
-        }
-    });
-    
-    function updateMessages(data) {
-        console.log("updating messages now");
-        var messages=data['messages'];
-        setMessages(messages);
-        var newest_message = document.getElementById('msg_' + (messages.length-1));
-        if(messages.length > 0){
-            newest_message.scrollIntoView();
-        }
+export default function MessageList() {
+  const [messages, setMessages] = React.useState(() => {
+    Socket.emit('get messages');
+    return [];
+  });
+
+  function updateMessages(data) {
+    const { msg } = data;
+    setMessages(msg);
+    const newestMessage = document.getElementById(`msg_${msg.length - 1}`);
+    if (msg.length > 0) {
+      newestMessage.scrollIntoView();
     }
-    
-    return (
-        <div className='messagelist'>
-            <ul>
-            {
+  }
+
+  React.useEffect(() => {
+    Socket.on('messages updated', updateMessages);
+    return () => {
+      Socket.off('messages updated', updateMessages);
+    };
+  });
+
+  return (
+    <div className="messagelist">
+      <ul>
+        {
                 (messages || []).map(
-                (message, index) => 
-                    <li key={'msg_' + index}>
-                        <div className='message' id={'msg_' + index}>
-                            <p className='user'><b>{message['user']}</b></p>
-                            <div className='messagebox'>
-                                <p className='messagetext'>{Parser(message['message'])}</p>
-                                <p className='timestamp'><i>{message['timestamp']}</i></p>
-                            </div>
+                  (message, index) => (
+                    <li key={`msg_${index}`}>
+                      <div className="message" id={`msg_${index}`}>
+                        <p className="user"><b>{message.user}</b></p>
+                        <div className="messagebox">
+                          <p className="messagetext">{Parser(message.message)}</p>
+                          <p className="timestamp"><i>{message.timestamp}</i></p>
                         </div>
+                      </div>
                     </li>
+                  ),
                 )
-            }   
-            </ul>
-        </div>
-    );
+            }
+      </ul>
+    </div>
+  );
 }
